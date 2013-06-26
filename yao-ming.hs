e48c9a6f05597239880880cc9e459e79f73e293c
Gísli Kristjánsson <gislik@hamstur.is>
2013-06-26 12:11:17 +0000

    String instances converted to Text

--- yao-ming.hs
+++ yao-ming.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 {- Yao Ming is a simple HTTP rebounder which sends a HTTP 301 response no matter what the request was:
@@ -12,40 +14,50 @@ import Network              (PortID(..), PortNumber, withSocketsDo, listenOn, ac
 import Network.Socket       (Socket, close)
 import Control.Concurrent   (forkIO)
 import Control.Applicative  ((<$>))
+import Control.Monad        (unless)
 import Control.Exception    (bracket)
 import System.Posix         (Handler(Ignore), installHandler, sigPIPE)
 import System.Environment   (getArgs)
-import Data.Maybe           (fromMaybe, listToMaybe)
-import System.IO            (Handle, hPutStrLn, hFlush, hClose)
+import Data.Maybe           (listToMaybe)
+import Data.Text            (Text, unwords, pack, null, unlines, append, strip)
+import Data.Text.IO         (hGetLine, hPutStrLn, putStrLn)
+import System.IO            (Handle, hFlush, hClose)
+import Prelude hiding       (putStrLn, putStr, unwords, null, unlines)
 
 -- configuration
 defaultPort :: PortNumber
 defaultPort = 8080
 
-programName :: String
+programName :: Text
 programName = "yao-ming"
 
-programVersion :: String
+programVersion :: Text
 programVersion = "0.0.2"
 
 -- main
 main :: IO ()
 main = withSocketsDo $ do
-   putStrLn $ unwords ["Starting", programName,  "version", programVersion, "on port", show defaultPort]
+   putStrLn $ unwords ["Starting", programName,  "version", programVersion, "on port", pack (show defaultPort)]
+   {- putStrLn $ unwords [programName, programVersion] --, show defaultPort] -}
    installHandler sigPIPE Ignore Nothing
-   url <- fromMaybe "http://example.com/" <$> listToMaybe <$> getArgs
+   url <- maybe "http://example.com/" pack <$> listToMaybe <$> getArgs
    bracket 
       (acquireSocket $ PortNumber defaultPort)
       closeSocket
       (flip acceptConnection $ redirectConnection url)
 
-redirectConnection :: String -> Handle -> IO ()
-redirectConnection url h = hPutStrLn h (constructResponse url) >> hFlush h >> hClose h
+redirectConnection :: Text -> Handle -> IO ()
+redirectConnection url h = readRequest h >> hPutStrLn h (constructResponse url) >> hFlush h >> hClose h
+   
+readRequest :: Handle -> IO ()
+readRequest h = do
+   line <- strip <$> hGetLine h 
+   unless (null line) (readRequest h)
 
 -- helpers
-constructResponse :: String -> String
+constructResponse :: Text -> Text
 constructResponse url = unlines ["HTTP/1.1 301 Moved Permanently"
-                                ,"Location: " ++ url
+                                ,append "Location: " url
                                 ,"Content-Length: 0"]
 
 acceptConnection :: Socket -> (Handle -> IO ()) -> IO ()
